f84a7a50c3d5962c2a96249a42f3f5fb334e0619
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-10-26 11:21:25 +0700

    3.6-barrier: Extract class Barrier

--- 3.6-barrier.rb
+++ 3.6-barrier.rb
@@ -15,18 +15,28 @@ class AtomicIncrementer
   end
 end
 
-turnstile = Semaphore.new(0)
+class Barrier
+  def initialize(number_of_threads)
+    @number_of_threads = number_of_threads
+    @number_of_threads_at_barrier = AtomicIncrementer.new(0)
+    @turnstile = Semaphore.new(0)
+  end
+
+  def barricade
+    @number_of_threads_at_barrier.increment
+    @turnstile.release if @number_of_threads_at_barrier.count_is_at?(@number_of_threads)
+    @turnstile.acquire
+    @turnstile.release
+  end
+end
 
 number_of_threads = 3
-number_of_threads_at_barrier = AtomicIncrementer.new(0)
+barrier = Barrier.new(number_of_threads)
 
 number_of_threads.times do |i|
   async do
     event "before barrier: #{i}"
-    number_of_threads_at_barrier.increment
-    turnstile.release if number_of_threads_at_barrier.count_is_at?(number_of_threads)
-    turnstile.acquire
-    turnstile.release
+    barrier.barricade
     event "after barrier: #{i}"
   end
 end
